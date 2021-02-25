80cd730a203cd7906bc8a19fe507dc4aa0fa7b24
Seiji Sawayanagi <sawayanagi@1101.com>
2021-02-25 18:17:39 +0900

    Refactoring

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 const App = (props) => {
   const [count, setCount] = useState(0);
 
-  const handleCount = () => {
-    setCount(count + 1);
+  const handleCount = (e) => {
+    setCount(count + Number(e.target.value));
   };
 
   return (
     <>
       <h1>{props.title} click {count} times</h1>
-      <button type="button" onClick={handleCount}>click</button>
+      <button type="button" onClick={handleCount} value="1">click</button>
     </>
   );
 };
